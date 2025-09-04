7498f4d6a3dfdbce517d7bcdd75cbc48ea8bf6c4
Drate Hillary <dratehillary@gmail.com>
2025-09-04 19:25:42 +0300

    Solved

--- cropdisease/agronomist_app/urls.py
+++ cropdisease/agronomist_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
 from .views import agronomist_home
-from authentication.views import sign_out
+from authentication.views import sign_out, edit_profile
 
 urlpatterns = [
     path('', agronomist_home, name='agronomist_home'),
     path('sign_out/', sign_out, name='sign_out'),
+    path('editProfile', edit_profile, name='edit_profile'),
 ]
